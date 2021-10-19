7bd2ff6601cc4ddba0749669e871db5713ca6438
Pablo Puga <pablo.puga.git@gmail.com>
2021-10-19 17:34:42 +0200

    Added decimal.js and implemented Leibniz, Nilakantha and Wallis algorithms

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,35 @@
 <script>
+    import Decimal from 'decimal.js';
+
+    import { nilakantha, leibniz, wallis } from './algorithms';
+    
+    const pi = '3.1415926535897932384626433832795028841971693993751';
+    const precision = 50;
+    const iterations = 1000000;
+
+    Decimal.set({ precision });
+
+    const nilakanthaGenerator = nilakantha();
+    const leibnizGenerator = leibniz();
+    const wallisGenerator = wallis();
+    
+    const nilakanthaList = [];
+    const leibnizList = [];
+    const wallisList = [];
+
+    for (let it = 0; it < iterations; ++it) {
+        nilakanthaList.push(nilakanthaGenerator.next().value);
+        leibnizList.push(leibnizGenerator.next().value);
+        wallisList.push(wallisGenerator.next().value);
+    }
+
+    console.log({
+        ni: nilakanthaList[nilakanthaList.length - 1],
+        le: leibnizList[leibnizList.length - 1],
+        wa: wallisList[wallisList.length - 1],
+        pi
+    });
+
 	let name = 'world';
 </script>
 
---
+++ src/algorithms.js
@@ -0,0 +1,41 @@
+import Decimal from 'decimal.js';
+
+export const leibniz = function *() {
+    let pi = new Decimal(0);
+    let sign = 1;
+    let next = 1;
+
+    while (true) {
+        const addend = (new Decimal(sign)).div(next);
+        pi = pi.add(addend);
+        sign = -sign;
+        next += 2;
+        yield pi.mul(4).toPrecision(Decimal.precision);
+    }
+};
+
+export const nilakantha = function *() {
+    let pi = new Decimal(3);
+    let sign = 1;
+    let next = 2;
+
+    while (true) {
+        const addend = (new Decimal(sign)).mul(4).div(new Decimal(next * (next + 1) * (next + 2)));
+        pi = pi.add(addend);
+        sign = -sign;
+        next += 2;
+        yield pi.toPrecision(Decimal.precision);
+    }
+};
+
+export const wallis = function *() {
+    let pi = new Decimal(1);
+    let next = 1;
+
+    while (true) {
+        const twiceNext = 2 * next++;
+        const multiplier = (new Decimal(twiceNext)).div(twiceNext - 1).mul(twiceNext).div(twiceNext + 1);
+        pi = pi.mul(multiplier);
+        yield pi.mul(2).toPrecision(Decimal.precision);
+    }
+};
