07c3569b256dcd3b229ab5576e19459cf268973a
Pablo Puga <pablo.puga.git@gmail.com>
2021-10-20 14:06:56 +0200

    Defered the lists computations after mount

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,58 @@
 <script>
     import Decimal from 'decimal.js';
+    import { onMount } from 'svelte';
 
     import { nilakantha, leibniz, wallis, viete } from './algorithms';
     
     const pi = '3.1415926535897932384626433832795028841971693993751';
     const precision = 50;
-    const iterations = 1000000;
+    const iterations = 100000;
 
     Decimal.set({ precision });
 
-    const nilakanthaGenerator = nilakantha();
-    const leibnizGenerator = leibniz();
-    const wallisGenerator = wallis();
-    const vietesGenerator = viete();
-    
-    const nilakanthaList = [];
-    const leibnizList = [];
-    const wallisList = [];
-    const vieteList = [];
+    let nilakanthaList = [];
+    let leibnizList = [];
+    let wallisList = [];
+    let vieteList = [];
+    let ready = false;
+    let duration = 0;
 
-    for (let it = 0; it < iterations; ++it) {
-        nilakanthaList.push(nilakanthaGenerator.next().value);
-        leibnizList.push(leibnizGenerator.next().value);
-        wallisList.push(wallisGenerator.next().value);
-        vieteList.push(vietesGenerator.next().value);
-    }
-    nilakanthaGenerator.return(undefined);
-    leibnizGenerator.return(undefined);
-    wallisGenerator.return(undefined);
-    vietesGenerator.return(undefined);
+    onMount(async () => {
+        console.log('start');
+        const start = (new Date()).getTime();
+        const nilakanthaGenerator = nilakantha();
+        const leibnizGenerator = leibniz();
+        const wallisGenerator = wallis();
+        const vietesGenerator = viete();
+        
+        let nilakanthaListTmp = [];
+        let leibnizListTmp = [];
+        let wallisListTmp = [];
+        let vieteListTmp = [];
 
-    console.log({
+        for (let it = 0; it < iterations; ++it) {
+            nilakanthaListTmp.push(nilakanthaGenerator.next().value);
+            leibnizListTmp.push(leibnizGenerator.next().value);
+            wallisListTmp.push(wallisGenerator.next().value);
+            vieteListTmp.push(vietesGenerator.next().value);
+        }
+
+        nilakanthaGenerator.return(undefined);
+        leibnizGenerator.return(undefined);
+        wallisGenerator.return(undefined);
+        vietesGenerator.return(undefined);
+
+        nilakanthaList = nilakanthaListTmp;
+        leibnizList = leibnizListTmp;
+        wallisList = wallisListTmp;
+        vieteList = vieteListTmp;
+
+        duration = ((new Date()).getTime()) - start;
+        ready = true;
+        console.log('end');
+    });
+
+    $: console.log(ready, duration, {
         ni: nilakanthaList[nilakanthaList.length - 1],
         le: leibnizList[leibnizList.length - 1],
         wa: wallisList[wallisList.length - 1],
