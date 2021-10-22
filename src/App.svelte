447888be6f77145fd7b7bec87e430fb1c5a6de8c
Pablo Puga <pablo.puga.git@gmail.com>
2021-10-22 18:52:58 +0200

    Updated rollup options in dev and localized the printing of the iterations

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,15 @@
     const iterations = 100000;
 </script>
 
-<article>
+<article class="p-3 md:p-2 lg:p-0 md:w-3/4 max-w-screen-md">
     <h1>Pi Convergence</h1>
     <h2>Showcase of different algorithms' convergence</h2>
     <p>
         This is a showcase of the convergence of different infinite series in the calculus of number Pi (π).
-        For this example, {iterations} different values are calculated for each formula with a precision of {precision}.
+        For this example, {iterations.toLocaleString()} different values are calculated for each formula with a precision of {precision}.
     </p>
     <p>
-        You can see the evolution of the series by adjusting the slider or by writting down an iteration number between 1 and {iterations}.
+        You can see the evolution of the series by adjusting the slider or by writting down an iteration number between 1 and {iterations.toLocaleString()}.
     </p>
 </article>
 <AlgorithmsTable {iterations} {precision}/>
