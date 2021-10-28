8cce4120e31ae68624591a7d1c13f04523c7c9ab
Pablo Puga <pablo.puga.git@gmail.com>
2021-10-28 14:34:00 +0200

    Added a transition effect and an on:click delegation to the FaIcon component

--- src/FaIcon.svelte
+++ src/FaIcon.svelte
@@ -7,15 +7,16 @@
     const cssClass = classes ? `fa-icon ${classes}` : 'fa-icon';
 </script>
 
-<svg aria-hidden="true" class={cssClass} role="img" xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
+<svg on:click aria-hidden="true" class={cssClass} role="img" xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
     <path fill="currentColor" d={path}/>
 </svg>
 
-<style>
+<style lang="postcss">
     .fa-icon {
         overflow: hidden;
         width: 1em;
         height: 1em;
         display: inline-block;
+        @apply transition duration-200;
     }
 </style>
